196f81ea3fa261c575869d03d88694707b24cbd4
Jesse Jonas <jessejonas13@gmail.com>
2022-04-04 22:46:14 -0300

    feat: added apollo client

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { client } from './lib/apollo';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
